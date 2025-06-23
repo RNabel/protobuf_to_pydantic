96b8ee979e16978249cfdf6af8e6ed0e085cf01c
Robin Nabel <robin@poolside.ai>
2025-06-23 16:52:55 +0100

    Remove v1 module files and dependency declarations

--- protobuf_to_pydantic/customer_validator/__init__.py
+++ protobuf_to_pydantic/customer_validator/__init__.py
@@ -1,8 +1,2 @@
-from protobuf_to_pydantic._pydantic_adapter import is_v1
-
-if is_v1:
-    from .v1 import *  # noqa
-else:
-    from .v2 import *  # noqa
-
+from .v2 import *  # noqa
 from .rule import set_now_default_factory
